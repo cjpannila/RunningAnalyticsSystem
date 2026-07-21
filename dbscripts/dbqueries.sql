0c95d11e96919840bbe99532f7798934c9ff48a3
cjpannila <chinthanapannila@gmail.com>
2026-07-21 13:18:54 +1000

    dbqueries and postman collection updates

--- dbscripts/dbqueries.sql
+++ dbscripts/dbqueries.sql
@@ -1,10 +1,13 @@
 Select * from users;
-Select count(*) from activities where user_id = 23177662;--Chinthana
-Select count(*) from activities where user_id = 105913375;--Imadi
-Select count(*) from activities where user_id = 98093514;--Anup
+-- 23177662 Chinthana 
+-- 98093514 Anup
+-- 113047789 Cher
+-- 614320 Steven
+
+Select count(*) from activities where user_id = 23177662;
 Select * from activities limit 10;
 
-Select count(*) from activities where user_id = 98093514 and avg_heartrate_bpm is not null and avg_cadence is not null;
+Select count(*) from activities where user_id = 614320 and avg_heartrate_bpm is not null and avg_cadence is not null;
 
 select * from weekly_summary where user_id = 98093514 order by week_start desc;
 --delete from weekly_summary where user_id = 98093514;
@@ -15,4 +18,5 @@ select * from weekly_summary where user_id = 98093514 order by week_start desc;
 
 select count(*) from weekly_summary where user_id = 23177662
 select * from user_clubs;
+Select * from clubs;
 
