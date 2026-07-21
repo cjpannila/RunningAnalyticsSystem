553c5fe67600f79382afbaa8384ec52ab3bf6975
cjpannila <chinthanapannila@gmail.com>
2026-07-21 15:03:02 +1000

    dbqueries updated

--- dbscripts/dbqueries.sql
+++ dbscripts/dbqueries.sql
@@ -3,11 +3,12 @@ Select * from users;
 -- 98093514 Anup
 -- 113047789 Cher
 -- 614320 Steven
+-- 7211094 Alan
+-- 13419295 Pradeep
 
-Select count(*) from activities where user_id = 23177662;
 Select * from activities limit 10;
-
-Select count(*) from activities where user_id = 614320 and avg_heartrate_bpm is not null and avg_cadence is not null;
+Select count(*) from activities where user_id = 7211094;
+Select count(*) from activities where user_id = 7211094 and avg_heartrate_bpm is not null and avg_cadence is not null;
 
 select * from weekly_summary where user_id = 98093514 order by week_start desc;
 --delete from weekly_summary where user_id = 98093514;
@@ -17,6 +18,6 @@ select * from weekly_summary where user_id = 98093514 order by week_start desc;
 -- 204 Anup > 132 
 
 select count(*) from weekly_summary where user_id = 23177662
-select * from user_clubs;
+select * from user_clubs where user_id = 7211094;
 Select * from clubs;
 
