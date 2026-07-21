89692b5c06306ffcb18ba486d9120a4a8f5e3466
cjpannila <chinthanapannila@gmail.com>
2026-07-21 20:47:24 +1000

    Db queries updated

--- dbscripts/dbqueries.sql
+++ dbscripts/dbqueries.sql
@@ -6,22 +6,26 @@ Select * from users;
 -- 7211094 Alan
 -- 13419295 Pradeep
 -- 32658279 Greg
+-- 27736141 David
+-- 7016611 ShunNai Zhao
 
---update users set country = 'Australia' where user_id = 32658279;
+--update users set country = 'Melbourne' where user_id = 27736141;
+--update users set city = 'Australia' where user_id = 27736141;
 
 Select * from activities limit 10;
-Select count(*) from activities where user_id = 13419295;
-Select count(*) from activities where user_id = 13419295 and activity_type = 'Run';
-Select count(*) from activities where user_id = 13419295 and avg_heartrate_bpm is not null and avg_cadence is not null and activity_type = 'Run';
+Select count(*) from activities where activity_type = 'Run' and distance_m > 0;
+Select count(*) from activities where user_id = 32658279;
+Select count(*) from activities where user_id = 32658279 and activity_type = 'Run';
+Select count(*) from activities where user_id = 32658279 and avg_heartrate_bpm is not null and avg_cadence is not null and activity_type = 'Run';
 
-select * from weekly_summary where user_id = 98093514 order by week_start desc;
+select * from weekly_summary where user_id = 32658279 order by week_start desc;
 --delete from weekly_summary where user_id = 98093514;
 
 -- weekly_summary previous
 -- 439 Chinthana > 155
 -- 204 Anup > 132 
 
-select count(*) from weekly_summary where user_id = 23177662
-select * from user_clubs where user_id = 7211094;
+select count(*) from weekly_summary where user_id = 32658279
+select * from user_clubs where user_id = 32658279;
 Select * from clubs;
 
