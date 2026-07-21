0195114a994c4bc651eaa8ba7b36db66e8b13761
cjpannila <chinthanapannila@gmail.com>
2026-07-21 17:36:21 +1000

    Db query updated for new user

--- dbscripts/dbqueries.sql
+++ dbscripts/dbqueries.sql
@@ -5,10 +5,14 @@ Select * from users;
 -- 614320 Steven
 -- 7211094 Alan
 -- 13419295 Pradeep
+-- 32658279 Greg
+
+--update users set country = 'Australia' where user_id = 32658279;
 
 Select * from activities limit 10;
-Select count(*) from activities where user_id = 7211094;
-Select count(*) from activities where user_id = 7211094 and avg_heartrate_bpm is not null and avg_cadence is not null;
+Select count(*) from activities where user_id = 13419295;
+Select count(*) from activities where user_id = 13419295 and activity_type = 'Run';
+Select count(*) from activities where user_id = 13419295 and avg_heartrate_bpm is not null and avg_cadence is not null and activity_type = 'Run';
 
 select * from weekly_summary where user_id = 98093514 order by week_start desc;
 --delete from weekly_summary where user_id = 98093514;
